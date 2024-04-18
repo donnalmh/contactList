f4406e80612491eee27b2de6184c618cecc3ea01
donnasamuel <donna@elenjicalsolutions.com>
2024-04-18 21:12:12 +0200

    Styling Contact form

--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -1,21 +1,32 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
-
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import '../../assets/poppins-font.css'
 @Component({
   selector: 'app-contact-form',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, NgIf],
   templateUrl: './contact-form.component.html',
   styleUrl: './contact-form.component.css'
 })
 export class ContactFormComponent {
+
+
   contactForm = new FormGroup({
-    name: new FormControl(''),
+    name: new FormControl('',[
+      Validators.required,
+    ]),
     surname: new FormControl(''),
     email: new FormControl(''),
     phoneNumber: new FormControl(''),
     dob: new FormControl('')
   })
+
+  nameFC = this.contactForm.get('name')
+  surnameFC = this.contactForm.get('surname')
+  emailFC = this.contactForm.get('email')
+  phoneNumberFC = this.contactForm.get('phoneNumber')
+  dobFC = this.contactForm.get('dob')
 
   onClick() {
     
--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,6 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import "ag-grid-community/styles/ag-grid.css";
+import './assets/poppins-font.css'
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
