af1c2427fb46fbd3345525687dea939a87ffd33a
donnasamuel <donna@elenjicalsolutions.com>
2024-04-19 11:11:22 +0200

    configure api call

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,17 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ContactListComponent } from './contact-list/contact-list.component';
 import { ContactFormComponent } from './contact-form/contact-form.component';
+import { ContactListService } from './shared/contact-list.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, ContactListComponent, ContactFormComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  providers:[
+    ContactListService]
 })
 export class AppComponent {
   title = 'ContactListApp';
--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -2,34 +2,53 @@ import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import '../../assets/poppins-font.css'
+import { ContactListService } from '../shared/contact-list.service';
+import { Contact } from '../shared/contact-list.model';
 @Component({
   selector: 'app-contact-form',
   standalone: true,
   imports: [ReactiveFormsModule, NgIf],
   templateUrl: './contact-form.component.html',
-  styleUrl: './contact-form.component.css'
+  styleUrl: './contact-form.component.css',
+  providers: [ContactListService]
 })
 export class ContactFormComponent {
 
+  constructor(private service:ContactListService) {}
 
   contactForm = new FormGroup({
     name: new FormControl('',[
       Validators.required,
     ]),
     surname: new FormControl(''),
-    email: new FormControl(''),
-    phoneNumber: new FormControl(''),
-    dob: new FormControl('')
+    emailAddress: new FormControl(''),
+    contactNumber: new FormControl(0),
+    dateOfBirth: new FormControl(undefined)
   })
 
   nameFC = this.contactForm.get('name')
   surnameFC = this.contactForm.get('surname')
-  emailFC = this.contactForm.get('email')
-  phoneNumberFC = this.contactForm.get('phoneNumber')
-  dobFC = this.contactForm.get('dob')
+  emailAddressFC = this.contactForm.get('emailAddress')
+  contactNumberFC = this.contactForm.get('contactNumber')
+  dateOfBirthFC = this.contactForm.get('dateOfBirth')
 
+  formatData(): Contact {
+    return new Contact(
+      this.nameFC?.value || '',
+      this.surnameFC?.value || '',
+      this.contactNumberFC?.value || 0,
+      this.emailAddressFC?.value || '',
+      this.dateOfBirthFC?.value || new Date()
+    )
+  }
   onClick() {
-    
+    const data = this.formatData()
+    this.service.postContactDetail(data).subscribe(
+      res => {
+        console.log(res)
+      },
+      err => { console.log(err)}
+    )
   }
 
 }
