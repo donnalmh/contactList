8a1dca8368562b15d11bac3fbdd8dcb88ca841d1
donnasamuel <donna@elenjicalsolutions.com>
2024-04-19 11:11:35 +0200

    create submission modal

---
+++ src/app/contact-form/modal/submission-modal/submission-modal.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-submission-modal',
+  standalone: true,
+  imports: [],
+  templateUrl: './submission-modal.component.html',
+  styleUrl: './submission-modal.component.css'
+})
+export class SubmissionModalComponent {
+
+}
