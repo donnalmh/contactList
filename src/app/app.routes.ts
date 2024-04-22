d22375fca4f9785984b274ec3863df53af568b98
donnasamuel <donna@elenjicalsolutions.com>
2024-04-22 10:13:51 +0200

    add routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { ContactListComponent } from './contact-list/contact-list.component';
+import { ContactFormComponent } from './contact-form/contact-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: ContactListComponent},
+    { path: 'form', component: ContactFormComponent},
+    { path: 'form/:id', component: ContactFormComponent}
+];
