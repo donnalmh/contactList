f86ce9e6d3b280d85462164bcfcb1033f713925a
donnasamuel <donna@elenjicalsolutions.com>
2024-04-22 10:14:19 +0200

    correct form control

--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -1,36 +1,73 @@
 import { NgIf } from '@angular/common';
-import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import '../../assets/poppins-font.css'
+import { Component, OnInit } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import '../../assets/poppins-font.css';
 import { ContactListService } from '../shared/contact-list.service';
 import { Contact } from '../shared/contact-list.model';
+import { ActivatedRoute, RouterModule } from '@angular/router';
+import { concatMap, empty, filter } from 'rxjs';
+import moment from 'moment';
 @Component({
   selector: 'app-contact-form',
   standalone: true,
-  imports: [ReactiveFormsModule, NgIf],
+  imports: [ReactiveFormsModule, NgIf, RouterModule],
   templateUrl: './contact-form.component.html',
   styleUrl: './contact-form.component.css',
-  providers: [ContactListService]
+  providers: [ContactListService],
 })
-export class ContactFormComponent {
+export class ContactFormComponent implements OnInit {
+  constructor(
+    private service: ContactListService,
+    private route: ActivatedRoute
+  ) {}
 
-  constructor(private service:ContactListService) {}
+  id: number | undefined= undefined;
+  contactForm: FormGroup | undefined;
+  nameFC!: FormControl;
+  surnameFC!: FormControl;
+  emailAddressFC!: FormControl;
+  contactNumberFC!: FormControl;
+  dateOfBirthFC!: FormControl;
 
-  contactForm = new FormGroup({
-    name: new FormControl('',[
-      Validators.required,
-    ]),
-    surname: new FormControl(''),
-    emailAddress: new FormControl(''),
-    contactNumber: new FormControl(0),
-    dateOfBirth: new FormControl(undefined)
-  })
+  ngOnInit(): void {
+    this.contactForm = new FormGroup({
+      name: new FormControl('', [Validators.required]),
+      surname: new FormControl(''),
+      emailAddress: new FormControl(''),
+      contactNumber: new FormControl(0),
+      dateOfBirth: new FormControl(undefined),
+    });
 
-  nameFC = this.contactForm.get('name')
-  surnameFC = this.contactForm.get('surname')
-  emailAddressFC = this.contactForm.get('emailAddress')
-  contactNumberFC = this.contactForm.get('contactNumber')
-  dateOfBirthFC = this.contactForm.get('dateOfBirth')
+    this.nameFC = this.contactForm.get('name') as FormControl;
+    this.surnameFC = this.contactForm.get('surname') as FormControl;
+    this.emailAddressFC = this.contactForm.get('emailAddress') as FormControl;
+    this.contactNumberFC = this.contactForm.get('contactNumber') as FormControl;
+    this.dateOfBirthFC = this.contactForm.get('dateOfBirth') as FormControl;
+
+    this.route.queryParamMap
+      .pipe(
+        filter(data => !!data.get('id')),
+        concatMap((data1: any) => { 
+          this.id = parseInt(data1.get('id'))
+          return this.service.getContactDetail(this.id)
+        })
+        )
+      .subscribe((res: any) => {
+        this.nameFC.setValue(res.name);
+        this.surnameFC.setValue(res.surname);
+        this.id = res.id;
+        this.emailAddressFC.setValue(res.emailAddress);
+        this.contactNumberFC.setValue(res.contactNumber);
+        this.dateOfBirthFC.setValue(
+          moment(res.dateOfBirth).format('YYYY-MM-DD')
+        );
+      });
+  }
 
   formatData(): Contact {
     return new Contact(
@@ -38,17 +75,31 @@ export class ContactFormComponent {
       this.surnameFC?.value || '',
       this.contactNumberFC?.value || 0,
       this.emailAddressFC?.value || '',
-      this.dateOfBirthFC?.value || new Date()
-    )
+      this.dateOfBirthFC?.value || new Date() 
+    );
   }
   onClick() {
-    const data = this.formatData()
-    this.service.postContactDetail(data).subscribe(
-      res => {
-        console.log(res)
-      },
-      err => { console.log(err)}
-    )
+    const data = this.formatData();
+    if(this.id) {
+      console.log("this.id: ", this.id)
+      data.id = this.id
+      this.service.updateContactDetail(data).subscribe(
+        (res) => {
+          console.log(res);
+        },
+        (err) => {
+          console.log(err);
+        }
+      );
+    } else {
+      this.service.postContactDetail(data).subscribe(
+        (res) => {
+          console.log(res);
+        },
+        (err) => {
+          console.log(err);
+        }
+      );
+    }
   }
-
 }
